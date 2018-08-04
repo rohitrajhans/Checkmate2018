e340bf3a96e44d531cc8e05a4dfe26486aa100e7
Rohit Rajhans <rohitrajhans07@gmail.com>
2018-08-04 10:04:04 +0530

    temp map and player movement

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Todo :
  - Main character created and movements are applied. Need to create a sample map with some walkable areas and some non-walkable areas and restrict character movement accordingly.
  - Create sample map in index.html file.
  - Compress images.
+ - Currently sizing done in px. Try to make it more responsive.
--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
-/* Question card display */
+"use strict";
 
 var questionLinks = document.getElementById('question-list').children;
 var questionCards = document.getElementsByClassName('question-card');
 var objImage = document.getElementById("character");
-
+// var imgDiv = document.getElementsByClassName("img-container")[0];
+var road = document.getElementsByClassName("road");
+var playerPosition; // Global variable to store player location
 
 /******************** CHARACTER MOVEMENT **************************/
 
 function init() {
     objImage.style.position = 'absolute';
-    objImage.style.left = '0px';
+    objImage.style.left = '160px';
     objImage.style.top = '0px';
     objImage.style.height = '80px';
     objImage.style.width = 'auto';
 }
 
 function getKeyAndMove(e) {
+    // console.log("t");
+    // playerPosition = objImage.getBoundingClientRect();
     var key_code = e.which || e.keyCode;
+    
+    // Checks if character is within the road
+    const shouldMove = isEnclosed(
+        road, 
+        objImage
+    );
+
+    // getBoundingClientRect returns value in px, so do not MESS UP.
+    // If character is not within the road, then go back to last on road position
+    if(!shouldMove) {
+        objImage.style.left = playerPosition.left + 'px';
+        objImage.style.top = playerPosition.top + 'px';
+        return;
+    };
+
     switch (key_code) {
         case 37: //left arrow key
             moveLeft();
@@ -27,11 +46,56 @@ function getKeyAndMove(e) {
         case 39: //right arrow key
             moveRight();
             break;
-        case 40: //down arrow key
+        case 40: //down arrow key`
             moveDown();
             break;
     };
+
+    // Save character on road position
+    playerPosition = objImage.getBoundingClientRect();
 };
+
+// function isEnclosed(roadList, player) {
+//     const enc = Array.from(roadList)
+//     .map(road => road.getBoundingClientRect())
+//     .some(roadCoordinates => {
+//         const {left, right, top, bottom} = roadCoordinates;
+//         const {left: pLeft,
+//             right: pRight, 
+//             top: pTop, 
+//             bottom: pBottom} = player.getBoundingClientRect(); 
+//         // console.log(x, y, left, right, top, bottom);
+//         return pLeft >= left
+//         && pRight <= right
+//         && pTop >= top
+//         && pBottom <= bottom;
+//     })
+//     console.log(enc); 
+//     return enc;
+// }
+
+function isEnclosed(roadList, player) {
+    var enc = Array.from(roadList).map(function (road) {
+        return road.getBoundingClientRect();
+    }).some(function (roadCoordinates) {
+        var left = roadCoordinates.left,
+            right = roadCoordinates.right,
+            top = roadCoordinates.top,
+            bottom = roadCoordinates.bottom;
+
+        var _player$getBoundingCl = player.getBoundingClientRect(),
+            pLeft = _player$getBoundingCl.left,
+            pRight = _player$getBoundingCl.right,
+            pTop = _player$getBoundingCl.top,
+            pBottom = _player$getBoundingCl.bottom;
+        // console.log(pLeft, pTop, left, right, top, bottom);
+
+
+        return pLeft >= left && pRight <= right && pTop >= top && pBottom <= bottom;
+    });
+    // console.log(enc);
+    return enc;
+}
 
 function moveLeft() {
     if(parseInt(objImage.style.left) > 0)
